b3a006b7c75f7aef1d48baeb169c856446927229
Unknown <bszamody@gmail.com>
2019-10-20 13:42:56 +1100

    PackagA/B/C initail commit Plus Demo Project

---
+++ SwiftPM-Demo/SwiftPM-Demo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftPM-Demo
+//
+//  Created by Balazs Szamody on 20/10/19.
+//  Copyright © 2019 Fr3qFly Studio. All rights reserved.
+//
+
+import UIKit
+import PackageA
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
